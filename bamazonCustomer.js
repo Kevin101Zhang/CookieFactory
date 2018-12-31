c92aee27980863b0b4061618b1c800aa5bbe6192
Pinyin <kevin101zhang@gmail.com>
2018-12-31 18:32:59 -0500

    FinishPt1

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var Password = require("password.js");
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Pokezhang101",
+    password: Password.PASSWORD,
     database: "bamazon"
 })
 
